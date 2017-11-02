d84935a036adbc6bcb687bde79773de2e20c63c3
Ryan Hoffman <ryan@ryanhoffmanmusic.com>
2017-11-02 13:43:23 -0700

    add shebang, add run conditional, add main() function

--- bin/bsf2wrfchem.py
+++ bin/bsf2wrfchem.py
@@ -1,10 +1,18 @@
+#!/usr/bin/env python3
+
 import csv
 
-with open('fire_locations_20130817.csv', 'r') as csv_file:
-    csv_reader = csv.DictReader(csv_file)
+def main():
+    with open('fire_locations_20130817.csv', 'r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
 
-    #this function skips the first line, which in this case is the label for the column
+
+    #this function skips the first line, which in this case is the label for the column which is not necessary when using DictReader() instead of csv.reader()
     #next(csv_reader)
 
-    for line in csv_reader:
-        print(line['latitude'])
+
+        for line in csv_reader:
+            print(line['latitude'])
+
+if __name__ == '__main__':
+    main()
