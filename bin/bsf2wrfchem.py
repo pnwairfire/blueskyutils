cae433c2a04524bd2be65f91481dc7e09552c2ec
Ryan Hoffman <ryan@ryanhoffmanmusic.com>
2017-11-10 16:25:58 -0800

    add argparse to select file

--- bin/bsf2wrfchem.py
+++ bin/bsf2wrfchem.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python3
 
 import csv
-#import os
+import os
+import argparse
 
-
+parser = argparse.ArgumentParser(description='Choose file: integer 0 - 6')
+parser.add_argument('-x', '--file_number', type=int, help='number of file')
+args = parser.parse_args()
 
 path = '/Users/rhoffman/code/airfire_misc/susan/blueskyutils/test/data/bsf2wrfchem_data/'
 files = [
-            'fire_locations_20130817.csv',
-            'fire_locations_20130818.csv',
-            'fire_locations_20130819.csv',
-            'fire_locations_20130820.csv',
-            'fire_locations_20130821.csv',
-            'fire_locations_20130822.csv',
-            'fire_locations_20130823.csv'
-            ]
+        'fire_locations_20130817.csv',
+        'fire_locations_20130818.csv',
+        'fire_locations_20130819.csv',
+        'fire_locations_20130820.csv',
+        'fire_locations_20130821.csv',
+        'fire_locations_20130822.csv',
+        'fire_locations_20130823.csv'
+        ]
 
 
-def main():
-    for f in files:
-        file = open(path +'%s' % f, 'r')
-        csv_reader = csv.DictReader(file)
-        for line in csv_reader:
-                print(line)
+def main(x):
+    f = files[x]
+    file = open(path +'%s' % f, 'r')
+    csv_reader = csv.DictReader(file)
+    for line in csv_reader:
+        print(line)
 
 
 if __name__ == '__main__':
-    main()
+    main(args.file_number)
