84984df1aee52634485e4787843a12313f04c064
Joel Dubowy <jdubowy@gmail.com>
2017-11-28 14:22:41 -0800

    Add skeleton logic for converting bsf fire to finn and then to wrfchem

--- bin/bsf2wrfchem.py
+++ bin/bsf2wrfchem.py
@@ -25,17 +25,38 @@ def parse_args():
         help='WRF-Chem input csv file to generate')
     return parser.parse_args()
 
+def convert_bsf_to_finn(bsf_fire):
+    finn_fire = {"fake_finn_field_1": 1, "fake_finn_field_2": "sdf"}
+    # TODO: set fields in finn_fire based on waht's in bsf_fire
+    return finn_fire
+
+def convert_finn_to_wrfchem(finn_fire):
+    wrfchem_fire = {"fake_wrfchem_field_1": 312, "fake_wrfchem_field_2": "jsdflkj"}
+    # TODO: set fields in finn_fire based on waht's in bsf_fire
+    return wrfchem_fire
+
+
 def main(args):
+    # load bsf fires
     logging.debug("Opening %s", args.fire_locations_input_file)
     with open(args.fire_locations_input_file, 'r') as file:
-        csv_reader = csv.DictReader(file)
+        bsf_fires = [f for f in csv.DictReader(file)]
 
-        n = 5
-        line = next((x for i, x in enumerate(csv_reader) if i == n), None)
-        print(line['pm25'])
+    # convert fires from bsf format to finn
+    finn_fires = [convert_bsf_to_finn(f) for f in bsf_fires]
+    with open(args.finn_input_file, 'w') as finn_output_file:
+        writer = csv.DictWriter(finn_output_file, list(finn_fires[0].keys()))
+        writer.writeheader()
+        writer.writerows(finn_fires)
 
-        #for line in csv_reader:
-            #print(line)
+    # convert fires from finn format to wrfchem if user specified
+    # wrfchem input file
+    if args.wrf_chem_input_file:
+        wrfchem_fires = [convert_finn_to_wrfchem(f) for f in finn_fires]
+        with open(args.wrf_chem_input_file, 'w') as wrfchem_output_file:
+            writer = csv.DictWriter(wrfchem_output_file, list(wrfchem_fires[0].keys()))
+            writer.writeheader()
+            writer.writerows(wrfchem_fires)
 
 if __name__ == '__main__':
     args = parse_args()
