40c1b9d428f5c73992ccaaa64d624f3e332d9939
Joel Dubowy <jdubowy@gmail.com>
2017-12-01 09:45:03 -0800

    WIP on converting BSF fields to FINN

--- blueskyutils/bsf2wrfchem/__init__.py
+++ blueskyutils/bsf2wrfchem/__init__.py
@@ -2,15 +2,34 @@ import csv
 import logging
 
 from .speciation import BSF2FINN_SPECIATION_FACTORS
+from .fccs2genveg import FCCS2GENVEG
+
+M2_PER_ACRE = 1000000 / 247.105  # == 4046.8626697153
+KG_PER_TON = 907.185
 
 def convert_bsf_to_finn(bsf_fire):
-    finn_fire = {"fake_finn_field_1": 1, "fake_finn_field_2": "sdf"}
-    # TODO: set fields in finn_fire based on waht's in bsf_fire
+    finn_fire = {
+        'DAY': '', # TODO: fill in using file timestamp
+        'TIME': '', # TODO: not sure hot to fill in
+        'GENVEG': FCCS2GENVEG.get(bsf_fire['fccs_number'], ''),
+        'LATI': float(bsf_fire['latitude']),
+        'LONGI': float(bsf_fire['longitude']),
+        'AREA': float(bsf_fire['area']) * M2_PER_ACRE,
+        'PM25': float(bsf_fire['pm25']) * KG_PER_TON
+
+        # TODO: continue setting fields in finn_fire based on
+        #  what's in bsf_fire
+
+    }
+
+
     return finn_fire
 
 def convert_finn_to_wrfchem(finn_fire):
     wrfchem_fire = {"fake_wrfchem_field_1": 312, "fake_wrfchem_field_2": "jsdflkj"}
+
     # TODO: set fields in finn_fire based on waht's in bsf_fire
+
     return wrfchem_fire
 
 def convert(fire_locations_input_file, finn_input_file, wrf_chem_input_file):
