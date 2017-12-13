cf6ccb5797a862a32ae9365801e0bac18c10d35e
Joel Dubowy <jdubowy@gmail.com>
2017-12-13 10:43:42 -0800

    Check each value separately to facilitate fixing one species at a time

--- test/unit/blueskyutils/bsf2wrfchem/test_bsf2wrfchem.py
+++ test/unit/blueskyutils/bsf2wrfchem/test_bsf2wrfchem.py
@@ -9,4 +9,5 @@ class TestBsf2Finn(object):
         finn_fire = bsf2wrfchem.convert_bsf_to_finn(BSF_FIRES[0])
         # need to use pytest.approx because expected values
         # are truncated
-        assert pytest.approx(finn_fire, abs=1e-4) == FINN_FIRES[0]
+        for k in sorted(FINN_FIRES[0].keys()):
+            assert pytest.approx(finn_fire[k], abs=1e-4) == float(FINN_FIRES[0][k])
