d871da992c075e998e207adf32ee99e9b0039a6b
Ryan Hoffman <ryan@ryanhoffmanmusic.com>
2018-01-03 11:09:55 -0800

    commented out wrf conversion function

--- blueskyutils/bsf2wrfchem/__init__.py
+++ blueskyutils/bsf2wrfchem/__init__.py
@@ -71,12 +71,12 @@ def convert_bsf_to_finn(bsf_fire):
 
     return finn_fire
 
-def convert_finn_to_wrfchem(finn_fire):
-    wrfchem_fire = {"fake_wrfchem_field_1": 312, "fake_wrfchem_field_2": "jsdflkj"}
+# def convert_finn_to_wrfchem(finn_fire):
+#     wrfchem_fire = {"fake_wrfchem_field_1": 312, "fake_wrfchem_field_2": "jsdflkj"}
 
     # TODO: set fields in finn_fire based on waht's in bsf_fire
 
-    return wrfchem_fire
+    # return wrfchem_fire
 
 def convert(fire_locations_input_file, finn_input_file, wrf_chem_input_file):
     # load bsf fires
@@ -105,11 +105,13 @@ def convert(fire_locations_input_file, finn_input_file, wrf_chem_input_file):
         writer.writeheader()
         writer.writerows(finn_fires)
 
+
+
     # convert fires from finn format to wrfchem if user specified
     # wrfchem input file
-    if wrf_chem_input_file:
-        wrfchem_fires = [convert_finn_to_wrfchem(f) for f in finn_fires]
-        with open(wrf_chem_input_file, 'w') as wrfchem_output_file:
-            writer = csv.DictWriter(wrfchem_output_file, list(wrfchem_fires[0].keys()))
-            writer.writeheader()
-            writer.writerows(wrfchem_fires)
+    # if wrf_chem_input_file:
+    #     wrfchem_fires = [convert_finn_to_wrfchem(f) for f in finn_fires]
+    #     with open(wrf_chem_input_file, 'w') as wrfchem_output_file:
+    #         writer = csv.DictWriter(wrfchem_output_file, list(wrfchem_fires[0].keys()))
+    #         writer.writeheader()
+    #         writer.writerows(wrfchem_fires)
